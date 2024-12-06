d423e8594afb3a5c325687eeffa6da664533c3fe
LilyanaOviPe <LilyanaOviPe@users.noreply.github.com>
2024-12-06 01:15:13 +0000

    Deploying to gh-pages from @ LilyanaOviPe/ui5-webcomponents@a3fd2414b26497b010accd18a0805c2aec163338 🚀

---
+++ nightly/assets/test/pages/ComponentStyles.html-6a5bd149.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-6b1465ed.js";const n="_inputIcon_1ojd7_2";const o=document.getElementById("custom-icon");o.classList.add(n);
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.5",
     isNext: false,
-    buildTime: 1733361135,
+    buildTime: 1733447481,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
