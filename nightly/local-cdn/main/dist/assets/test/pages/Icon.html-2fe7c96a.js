9bb2b8c428a70fcc47dc4bd3b5688d3534d0e13f
LilyanaOviPe <LilyanaOviPe@users.noreply.github.com>
2025-08-14 01:23:44 +0000

    Deploying to gh-pages from @ LilyanaOviPe/ui5-webcomponents@a3fd2414b26497b010accd18a0805c2aec163338 🚀

---
+++ nightly/local-cdn/main/dist/assets/test/pages/Icon.html-2fe7c96a.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-43a7b5de.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
