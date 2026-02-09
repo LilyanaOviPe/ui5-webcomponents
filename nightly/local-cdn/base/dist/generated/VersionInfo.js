943f089ac575923ad46508d83acf1a5e4b739784
LilyanaOviPe <LilyanaOviPe@users.noreply.github.com>
2026-02-09 01:59:36 +0000

    Deploying to gh-pages from @ LilyanaOviPe/ui5-webcomponents@a3fd2414b26497b010accd18a0805c2aec163338 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.5",
     isNext: false,
-    buildTime: 1770516710,
+    buildTime: 1770602145,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
